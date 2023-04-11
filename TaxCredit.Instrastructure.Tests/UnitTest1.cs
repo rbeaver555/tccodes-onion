408065416cc97b9371eb5a60e449fad54bc88c1b
rbeaver555 <ryanbeaver@gmail.com>
2023-04-11 14:29:28 -0400

    Upgraded branch to latest .net version

---
+++ TaxCredit.Instrastructure.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TaxCredit.Instrastructure.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
